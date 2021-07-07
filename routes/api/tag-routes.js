82fc26e8244663da85c153a9f77e81681c5fc54f
Camden Whirl <camdenwhirl@gmail.com>
2021-07-07 01:41:29 -0600

    tags put route functional

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -27,10 +27,24 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create( req.body
+  ).then(newTag => {
+    res.json(newTag)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {tag_name:req.body.tag_name},
+    {where: {id: req.params.id}}
+    ).then((newID) => {
+      res.json(newID)
+    }).catch(err => {
+      res.status(500).json(err)
+    })
 });
 
 router.delete('/:id', (req, res) => {
