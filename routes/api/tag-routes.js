a695866c9b9edd9f4e79d05816f31329b7116de4
Camden Whirl <camdenwhirl@gmail.com>
2021-07-07 01:21:14 -0600

    get tag and get tag by ID routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,23 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({ include:[Product]
+  }).then(allTags => {
+    res.json(allTags)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {include: [{ model: Product }],
+  }).then(tag => {
+    res.json(tag)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
 });
 
 router.post('/', (req, res) => {
