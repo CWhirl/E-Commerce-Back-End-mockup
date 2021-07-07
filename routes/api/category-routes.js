3af7beaef4942b0f9bd79cad839a059082ef2433
Camden Whirl <camdenwhirl@gmail.com>
2021-07-07 02:07:13 -0600

    delete category route functional

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,6 +49,12 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({where:{id:req.params.id}}
+    ).then((removeCategory) => {
+      res.json(removeCategory)
+    }).catch(err => {
+      res.status(500).json(err)
+    })
 });
 
 module.exports = router;
