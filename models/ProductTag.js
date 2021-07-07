c087fa28c361ea28cd60810940187f87ace0da2a
Camden Whirl <camdenwhirl@gmail.com>
2021-07-07 00:35:50 -0600

    models and seed functioning

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -5,9 +5,23 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-  },
+	{
+		// define columns
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		product_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'product',
+				key: 'id',
+				unique: false
+			},
+		}
+	},
   {
     sequelize,
     timestamps: false,
