3cef4feda23ec15f745435f39e15971cf9cb5eb7
Camden Whirl <camdenwhirl@gmail.com>
2021-07-07 01:56:19 -0600

    get categories route functional

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,11 +2,17 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({ include:[Product]
+  }).then(allCategories => {
+    res.json(allCategories)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
 });
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
